89b75d374963a26961c31dc5721ef38b12e0c2c7
Harclerode95 <38046711+Harclerode95@users.noreply.github.com>
2018-04-08 04:10:04 -0400

    Add files via upload

---
+++ CO_Sensor.py
@@ -0,0 +1,27 @@
+# the sensor has to be connected to pin 1 for power, pin 6 for ground
+# and pin 7 for signal(board numbering!).
+ 
+import time, sys
+import RPi.GPIO as GPIO
+
+
+class COSensor:
+    CO_PIN = 7     # Needs valid pin
+
+    def __init__(self):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.CO_PIN, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+        GPIO.add_event_detect(7, GPIO.RISING)
+        # GPIO.add_event_callback(7, action)
+
+    def __action__(self, pin):              # Parameters?
+        print('Sensor detected action!')
+        return
+
+    try:
+        while True:
+            print('alive')
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        sys.exit()
